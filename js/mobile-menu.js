03b3cb5acd339bceafb3bdc175911920a75b2ba1
Daria Shpak <darya.shpak@gmail.com>
2024-05-29 22:11:14 +0300

    Adds mobile-menu and updates js script

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -3,12 +3,20 @@
     openMenuBtn: document.querySelector("[data-menu-open]"),
     closeMenuBtn: document.querySelector("[data-menu-close]"),
     menu: document.querySelector("[data-menu]"),
-    body: document.querySelector("body")
+    body: document.querySelector("body"),
+     menuLinks: document.querySelectorAll(".mob-menu-nav-link")
   }
 
   refs.openMenuBtn.addEventListener("click", toggleMenu)
   refs.closeMenuBtn.addEventListener("click", toggleMenu)
 
+   refs.menuLinks.forEach((link) => {
+     link.addEventListener("click", () => {
+       refs.menu.classList.add("is-hidden")
+       refs.body.classList.remove("no-scroll")
+     })
+   })
+  
   function toggleMenu() {
     refs.menu.classList.toggle("is-hidden")
     refs.body.classList.toggle("no-scroll")
