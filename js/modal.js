1a99ee123510845ea1b67abfde4562d6fad86d71
Daria Shpak <darya.shpak@gmail.com>
2024-07-20 00:26:16 +0300

    Updates form-submit

--- js/modal.js
+++ js/modal.js
@@ -2,26 +2,24 @@ document.addEventListener("DOMContentLoaded", () => {
   const openFormModalButtons = document.querySelectorAll("[data-modal-open]")
   const formModal = document.querySelector("[data-modal]")
   const closeFormModalButton = document.querySelector("[data-modal-close]")
-
   const openAboutModalButton = document.querySelector("[data-about-modal-open]")
   const aboutModal = document.getElementById("about-modal")
   const closeAboutModalButton = document.querySelector(
     "[data-about-modal-close]"
   )
 
-  // Відкрити форму
   openFormModalButtons.forEach((button) => {
     button.addEventListener("click", () => {
+      const source = button.getAttribute("data-source")
+      document.getElementById("source-input").value = source
       formModal.classList.remove("is-hidden")
     })
   })
 
-  // Закрити форму
   closeFormModalButton.addEventListener("click", () => {
     formModal.classList.add("is-hidden")
   })
 
-  // Закриття модалки при натисканні на backdrop
   document.querySelectorAll(".backdrop").forEach((backdrop) => {
     backdrop.addEventListener("click", (e) => {
       if (e.target === backdrop) {
@@ -30,7 +28,6 @@ document.addEventListener("DOMContentLoaded", () => {
     })
   })
 
-  // Закриття модалок при натисканні на клавішу Escape
   document.addEventListener("keydown", (e) => {
     if (e.key === "Escape") {
       if (!formModal.classList.contains("is-hidden")) {
@@ -41,4 +38,42 @@ document.addEventListener("DOMContentLoaded", () => {
       }
     }
   })
+
+  document
+    .getElementById("my-form")
+    .addEventListener("submit", function (event) {
+      event.preventDefault()
+      const form = event.target
+      const formData = new FormData(form)
+
+      fetch("https://formspree.io/f/mpzvgkqe", {
+        method: "POST",
+        body: formData,
+        headers: {
+          Accept: "application/json"
+        }
+      })
+        .then((response) => {
+          if (response.ok) {
+            form.style.display = "none"
+            document.getElementById("successMessage").style.display = "block"
+            form.reset()
+          } else {
+            return response.json().then((data) => {
+              if (Object.hasOwn(data, "errors")) {
+                const errorMessage = data["errors"]
+                  .map((error) => error["message"])
+                  .join(", ")
+                throw new Error(errorMessage)
+              } else {
+                throw new Error("Unknown error occurred")
+              }
+            })
+          }
+        })
+        .catch((error) => {
+          document.getElementById("errorMessage").style.display = "block"
+          console.error("There was an error!", error)
+        })
+    })
 })
