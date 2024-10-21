19666020da880e6b8550db10c875f6c6a5a6bbf2
Daria Shpak <119341842+DariShpak@users.noreply.github.com>
2024-10-22 00:50:14 +0300

    Update mobile-menu.js

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,27 +1,4 @@
-// ;(() => {
-//   const refs = {
-//     openMenuBtn: document.querySelector("[data-menu-open]"),
-//     closeMenuBtn: document.querySelector("[data-menu-close]"),
-//     menu: document.querySelector("[data-menu]"),
-//     body: document.querySelector("body"),
-//      menuLinks: document.querySelectorAll(".mob-menu-nav-link")
-//   }
 
-//   refs.openMenuBtn.addEventListener("click", toggleMenu)
-//   refs.closeMenuBtn.addEventListener("click", toggleMenu)
-
-//    refs.menuLinks.forEach((link) => {
-//      link.addEventListener("click", () => {
-//        refs.menu.classList.add("is-hidden")
-//        refs.body.classList.remove("no-scroll")
-//      })
-//    })
-  
-//   function toggleMenu() {
-//     refs.menu.classList.toggle("is-hidden")
-//     refs.body.classList.toggle("no-scroll")
-//   }
-// })()
 
 
 const menuBtn = document.querySelector('.mob-menu-btn');
